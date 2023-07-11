f5d94c441b5a4b1e1c134328c1ecd2abc9c3d419
Alex <alex@brewingcode.net>
2023-07-11 15:33:54 -0700

    fix ledger line scaling

--- src/write/draw/staff-line.js
+++ src/write/draw/staff-line.js
@@ -2,6 +2,9 @@ var printLine = require('./print-line');
 
 function printStaffLine(renderer, x1, x2, pitch, klass, name, dy) {
 	var y = renderer.calcY(pitch);
+    if (name == 'ledger' && renderer.notelabels) {
+        x2 = x1 + (x2 - x1) * 1.33;
+    }
 	return printLine(renderer, x1, x2, y, klass, name, dy);
 }
 
