7b64fe90a3474a1ec4379aab8ed4a60ef4322cad
Alex <alex@brewingcode.net>
2023-08-02 21:49:55 -0700

    leave a console.log in but commented
    This is a very useful spot to log

--- src/write/draw/relative.js
+++ src/write/draw/relative.js
@@ -20,6 +20,7 @@ function drawRelativeElement(renderer, params, bartop) {
 	if (params.pitch === undefined)
 		window.console.error(params.type + " Relative Element y-coordinate not set.");
 	var y = renderer.calcY(params.pitch);
+	//console.log("drawRelativeElement:", params)
 	switch (params.type) {
 		case "symbol":
 			if (params.c === null) return null;
